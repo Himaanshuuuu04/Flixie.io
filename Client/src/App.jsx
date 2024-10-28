373b36990479a5c098d43ff923bcff6badc73a42
Himaansh_uu <himan0411singh@gmail.com>
2024-10-28 21:05:42 +0530

    Add background

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,20 +1,16 @@
-import './App.css'
-import { HashRouter, Routes, Route, useLocation } from "react-router-dom";
-import Home from './components/routes/Home.jsx'
-import NotFound from './components/NotFound.jsx';
-function App() {
-  <Routes location={location}>
-    <Route path="/" element={<Home />} />
- 
-    <Route path="*" element={<NotFound/>} />
-  </Routes>
 
+import { HashRouter, Routes, Route, useLocation } from "react-router-dom";
+import Home from './components/routes/Home.jsx';
+import NotFound from "./components/routes/NotFound.jsx";
+function App() {
   return (
-    <>
     <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </HashRouter> 
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Client/src/components/routes/Home.jsx
+++ Client/src/components/routes/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { BackgroundGradientAnimation } from '../Gradient'
 export default function Home() {
     return (
         <div>
-            <h1>Home</h1>
+            <BackgroundGradientAnimation/>
+           
         </div>
     )
 }
